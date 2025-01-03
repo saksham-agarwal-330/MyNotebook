5f615c7d98f561dcf36dd31b7081eea96927ebee
Saksham Agarwal <sakshamagarwal330@gmail.com>
2025-01-03 21:15:23 +0530

    fix1

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mynotebook', {});
+    await mongoose.connect('process.env.MONGO_URI;', {});
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error);
--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<SignUp />} />
         </Routes>
