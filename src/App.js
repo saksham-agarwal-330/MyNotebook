1a8ed977f893f24058a74f23cc2a4f1b5d506cfd
Saksham Agarwal <sakshamagarwal330@gmail.com>
2025-01-03 19:49:54 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NoteState from './context/notes/NoteState';
 import Login from './components/Login';
-import Signup from './components/SignUp';
+import SignUp from './components/SignUp';
 function App() {
   return (
     <div className="App">
@@ -14,8 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
           <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
         </NoteState>
